a842909c4e41c308bf95119c7ab5bdae9bf0a9b1
yj620 <yeongju620@gmail.com>
2023-02-10 17:13:06 +0900

    3번째 백업 태블릿사이즈 메뉴 제작중

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 $(function(){
+    // 소속 박물관 바로가기
     $('.museum-baro').on('click',function(){
         $('.museum-baro').addClass('on');
         $('.museum-baro-show').slideDown();
     }, function(){
         $('.museum-baro').removeClass('on');
-        $('.museum-baro-show').slideUp()
+        $('.museum-baro-show').slideUp();
     });
+
+    // 헤더 햄버거 메뉴
+    $('.header-menu').click(function(){
+        $('.header-menu').toggleClass('on');
+        
+        // 반응형 태블릿사이즈 메뉴
+        if($(this).hasClass('on')){
+            // $(this).parents('.header-r').next('.nav-t').show();
+            $('.nav-t').show(); 
+            
+        }else{
+            // $(this).parents('.header-r').next('.nav-t').hide();
+            $('.nav-t').hide();  
+        }
+    })
+
+    
 })
