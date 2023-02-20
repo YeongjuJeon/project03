0a0721cae0abb92612b3b16be958ee6de41c0d8d
YeongjuJeon <yjyj620@naver.com>
2023-02-20 18:20:08 +0900

    핸드폰사이즈 메뉴 작업중 반응형

--- js/script.js
+++ js/script.js
@@ -1,27 +1,69 @@
 $(function(){
-    // 소속 박물관 바로가기
-    $('.museum-baro').on('click',function(){
-        $('.museum-baro').addClass('on');
-        $('.museum-baro-show').slideDown();
-    }, function(){
-        $('.museum-baro').removeClass('on');
-        $('.museum-baro-show').slideUp();
-    });
+    $(window).resize(function(){ 
+        if (window.innerWidth < 600){
+            // 헤더 햄버거 메뉴
+            $('.header-menu').click(function(){
+                $('.header-menu').toggleClass('on');
+                
+                // 반응형 핸드폰사이즈 메뉴
+                if($(this).hasClass('on')){
+                    $('.nav-p').show(); 
+                    $('.nav-t').hide();
+                }else{
+                    $('.nav-p').hide();  
+                }
+            })
+            // 메뉴2depth 슬라이드다운
+            $('.P-depth1>li').click(function(e){
+                e.preventDefault();
+                if($(this).children('ul').css('display','none')){
+                    $(this).children('ul').slideDown();
+                }else{
+                    $(this).children('ul').slideUp();
+                }
+            })
 
-    // 헤더 햄버거 메뉴
-    $('.header-menu').click(function(){
-        $('.header-menu').toggleClass('on');
-        
-        // 반응형 태블릿사이즈 메뉴
-        if($(this).hasClass('on')){
-            // $(this).parents('.header-r').next('.nav-t').show();
-            $('.nav-t').show(); 
-            
+        //600px보다클때 
         }else{
-            // $(this).parents('.header-r').next('.nav-t').hide();
-            $('.nav-t').hide();  
-        }
-    })
 
+            // 헤더 햄버거 메뉴
+            $('.header-menu').click(function(){
+                $('.header-menu').toggleClass('on');
+                
+                // 반응형 태블릿사이즈 메뉴
+                if($(this).hasClass('on')){
+                    // $(this).parents('.header-r').next('.nav-t').show();
+                    $('.nav-t').show(); 
+                    
+                }else{
+                    // $(this).parents('.header-r').next('.nav-t').hide();
+                    $('.nav-t').hide();  
+                }
+            })
+
+            // 태블릿사이즈 메뉴2depth hover
+            $(".Td-depth1>li").hover(function(){
+                $(this).addClass("active").siblings().removeClass('active');
+            });
+                
+            // 태블릿사이즈메뉴 3depth slideDown
+            $('.Td-depth2>li').hover(function(){
+                $(this).find('ul').stop().slideDown();
+            },function(){
+                $(this).find('ul').stop().slideUp();
+            })
+            
+              // 소속 박물관 바로가기
+            $('.museum-baro').on('click',function(){
+                $('.museum-baro').addClass('on');
+                $('.museum-baro-show').slideDown();
+            }, function(){
+                $('.museum-baro').removeClass('on');
+                $('.museum-baro-show').slideUp();
+            });
+        }
     
+    }).resize();
+
+
 })
